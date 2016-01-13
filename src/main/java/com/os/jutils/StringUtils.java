c9d38c004ea4eaa54fe874eeae4078e3760c12fd
finnick <finnick@infintesys.my>
2016-01-13 14:55:46 +0800

    StringUtils工具类

---
+++ src/main/java/com/os/jutils/StringUtils.java
@@ -0,0 +1,16 @@
+package com.os.jutils;
+
+public class StringUtils {
+	/**
+	 * null转化为空字符串
+	 * 
+	 * @param str
+	 * @return "" 或 str
+	 */
+	public String nullToBlank(String str) {
+		if (str == null) {
+			return "";
+		}
+		return str;
+	}
+}

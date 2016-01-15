ef4a478bb544dc2f488b9e8b299d727fb57345bb
finnick <finnick@infintesys.my>
2016-01-15 19:01:20 +0800

    增加解压缩方法

--- src/main/java/com/os/jutils/ZipUtils.java
+++ src/main/java/com/os/jutils/ZipUtils.java
@@ -1,12 +1,15 @@
 package com.os.jutils;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.util.zip.CRC32;
+import java.util.zip.CheckedInputStream;
 import java.util.zip.CheckedOutputStream;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
 /**
@@ -36,6 +39,19 @@ public class ZipUtils {
 		String name = srcFile.getName();
 		String destPath = srcFile.getParent();
 		compress(srcFile, destPath, name);
+	}
+
+	/**
+	 * 压缩
+	 * 
+	 * @param srcPath
+	 *            源文件路径
+	 * @throws Exception
+	 */
+	public static void compress(String srcPath) throws Exception {
+		File srcFile = new File(srcPath);
+
+		compress(srcFile);
 	}
 
 	/**
@@ -77,19 +93,6 @@ public class ZipUtils {
 		}
 		String destFilePath = destPath + fileName + EXT;
 		compress(srcFile, new File(destFilePath));
-	}
-
-	/**
-	 * 压缩
-	 * 
-	 * @param srcPath
-	 *            源文件路径
-	 * @throws Exception
-	 */
-	public static void compress(String srcPath) throws Exception {
-		File srcFile = new File(srcPath);
-
-		compress(srcFile);
 	}
 
 	/**
@@ -179,5 +182,156 @@ public class ZipUtils {
 		bis.close();
 
 		zos.closeEntry();
+	}
+
+	/**
+	 * 解压缩
+	 * 
+	 * @param srcFile
+	 *            源文件
+	 * @throws Exception
+	 */
+	public static void decompress(File srcFile) throws Exception {
+		String basePath = srcFile.getParent();
+		decompress(srcFile, basePath);
+	}
+
+	/**
+	 * 文件 解压缩
+	 * 
+	 * @param srcPath
+	 *            源文件路径
+	 * @throws Exception
+	 */
+	public static void decompress(String srcPath) throws Exception {
+		File srcFile = new File(srcPath);
+
+		decompress(srcFile);
+	}
+
+	/**
+	 * 解压缩
+	 * 
+	 * @param srcFile
+	 *            源文件
+	 * @param destFile
+	 *            目标目录
+	 * @throws Exception
+	 */
+	public static void decompress(File srcFile, File destFile) throws Exception {
+
+		CheckedInputStream cis = new CheckedInputStream(new FileInputStream(srcFile), new CRC32());
+
+		ZipInputStream zis = new ZipInputStream(cis);
+
+		decompress(destFile, zis);
+
+		zis.close();
+
+	}
+
+	/**
+	 * 解压缩
+	 * 
+	 * @param srcFile
+	 *            源文件
+	 * @param destPath
+	 *            目标目录路径
+	 * @throws Exception
+	 */
+	public static void decompress(File srcFile, String destPath) throws Exception {
+		decompress(srcFile, new File(destPath));
+
+	}
+
+	/**
+	 * 文件 解压缩
+	 * 
+	 * @param srcPath
+	 *            源文件路径
+	 * @param destPath
+	 *            目标文件路径
+	 * @throws Exception
+	 */
+	public static void decompress(String srcPath, String destPath) throws Exception {
+
+		File srcFile = new File(srcPath);
+		decompress(srcFile, destPath);
+	}
+
+	/**
+	 * 文件 解压缩
+	 * 
+	 * @param destFile
+	 *            目标文件
+	 * @param zis
+	 *            ZipInputStream
+	 * @throws Exception
+	 */
+	private static void decompress(File destFile, ZipInputStream zis) throws Exception {
+
+		ZipEntry entry = null;
+		while ((entry = zis.getNextEntry()) != null) {
+
+			// 文件
+			String dir = destFile.getPath() + File.separator + entry.getName();
+
+			File dirFile = new File(dir);
+
+			// 文件检查
+			fileProber(dirFile);
+
+			if (entry.isDirectory()) {
+				dirFile.mkdirs();
+			} else {
+				decompressFile(dirFile, zis);
+			}
+
+			zis.closeEntry();
+		}
+	}
+
+	/**
+	 * 文件探针
+	 * 
+	 * 
+	 * 当父目录不存在时，创建目录！
+	 * 
+	 * 
+	 * @param dirFile
+	 */
+	private static void fileProber(File dirFile) {
+
+		File parentFile = dirFile.getParentFile();
+		if (!parentFile.exists()) {
+
+			// 递归寻找上级目录
+			fileProber(parentFile);
+
+			parentFile.mkdir();
+		}
+
+	}
+
+	/**
+	 * 文件解压缩
+	 * 
+	 * @param destFile
+	 *            目标文件
+	 * @param zis
+	 *            ZipInputStream
+	 * @throws Exception
+	 */
+	private static void decompressFile(File destFile, ZipInputStream zis) throws Exception {
+
+		BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(destFile));
+
+		int count;
+		byte data[] = new byte[BUFFER];
+		while ((count = zis.read(data, 0, BUFFER)) != -1) {
+			bos.write(data, 0, count);
+		}
+
+		bos.close();
 	}
 }
--- src/test/java/com/os/jutils/ZipUtilsTest.java
+++ src/test/java/com/os/jutils/ZipUtilsTest.java
@@ -4,22 +4,36 @@ import java.io.File;
 
 import org.junit.Test;
 
-public class ZipUtilsTest {  
-  
-    /** 
-     *   
-     */  
-    @Test  
-    public void test() throws Exception {  
-    	//指定源目录压缩
-    	// ZipUtils.compress("E:\\test");  
-    	//指定源目录，目标目录，目标文件名
-    	//ZipUtils.compress("E:\\test", "E:\\安装介质\\新建文件夹", "123");
-        //指定源文件压缩
-        //ZipUtils.compress(new File("E:\\test"));
-    	//指定源文件，目标文件
-    	//ZipUtils.compress(new File("E:\\test"), new File("E:\\安装介质\\新建文件夹\\444.zip"));
-    	//指定源文件，目标目标，目标文件名
-    	ZipUtils.compress(new File("E:\\test"), "E:\\安装介质\\新建文件夹", "999");
-    }  
-}  
+public class ZipUtilsTest {
+
+	/** 
+	 *   
+	 */
+	@Test
+	public void compressTest() throws Exception {
+		// 指定源目录压缩
+		// ZipUtils.compress("E:\\test");
+		// 指定源目录，目标目录，目标文件名
+		// ZipUtils.compress("E:\\test", "E:\\安装介质\\新建文件夹", "123");
+		// 指定源文件压缩
+		// ZipUtils.compress(new File("E:\\test"));
+		// 指定源文件，目标文件
+		// ZipUtils.compress(new File("E:\\test"), new File("E:\\安装介质\\新建文件夹\\444.zip"));
+		// 指定源文件，目标目标，目标文件名
+		ZipUtils.compress(new File("E:\\test"), "E:\\安装介质\\新建文件夹", "999");
+	}
+
+	@Test
+	public void decompressTest() throws Exception {
+		// 指定源文件解压
+		//ZipUtils.decompress(new File("E:\\安装介质\\新建文件夹\\123.zip"));
+		// 指定源文件路径解压
+		//ZipUtils.decompress("E:\\安装介质\\新建文件夹\\123.zip");
+		// 指定源文件，目标目录
+		//ZipUtils.decompress(new File("E:\\安装介质\\新建文件夹\\123.zip"), new File("E:\\安装介质"));
+		// 指定源文件，目标目录路径
+		//ZipUtils.decompress(new File("E:\\安装介质\\新建文件夹\\123.zip"), "E:\\安装介质\\新建文件夹");
+		//指定源文件路径，目标目录路径
+		ZipUtils.decompress("E:\\安装介质\\新建文件夹\\123.zip", "E:\\安装介质\\新建文件夹");
+	}
+}
